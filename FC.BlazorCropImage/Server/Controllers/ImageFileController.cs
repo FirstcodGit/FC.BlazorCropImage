41cfd9013116e7ff471434e654c0b4e5dd5b1293
FirstcodGit <csharprogrammer@gmail.com>
2020-08-28 03:30:54 +0300

    Add project files.

---
+++ FC.BlazorCropImage/Server/Controllers/ImageFileController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FC.BlazorCropImage.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageFileController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string response)
+        {
+            Base64ToImage(response);
+            return Ok(HttpStatusCode.OK);
+        }
+
+        private Image Base64ToImage(string base64String)
+        {
+            // Convert base 64 string to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            // Convert byte[] to Image
+            using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                Image image = Image.FromStream(ms, true);
+
+                image.Save(Path.Combine("wwwroot", "images") + image);
+                return image;
+            }
+        }
+    }
+}
